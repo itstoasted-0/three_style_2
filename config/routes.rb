151e0c62bd5e7c8241954dd0b42d198f909b0a72
itstoasted-0 <snow.w686@gmail.com>
2021-05-21 00:15:53 +0900

    css設計を変更

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
             passwords: "users/passwords",
   }
   devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+    get "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
   resources :posts
   resource :users, only: :show
