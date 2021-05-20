6ee79ac7e5b8d71c99cf18f5d029fb06be3d2f3a
itstoasted-0 <snow.w686@gmail.com>
2021-05-20 22:35:21 +0900

    ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: "pages#top"
-  devise_for :users
+  devise_for :users, controllers: {
+            registrations: "users/registrations",
+            passwords: "users/passwords",
+  }
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
   resources :posts
   resource :users, only: :show
 end
